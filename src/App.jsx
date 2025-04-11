63364a7853f86962518fe4585ed3c518d9e225fc
severogianluca <gianlucasevero300@gmail.com>
2025-04-11 17:41:51 +0200

    Feat: add card for actors

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import axios from "axios"
-import { use } from "react";
 import { useEffect, useState } from "react";
 
 function App() {
   const [actors, setActor] = useState([])
 
-  function fecthActor(){
-    axios.get("https://freetestapi.com/api/v1/actresses")
+  function fetchActor(){
+    axios.get("https://www.freetestapi.com/api/v1/actresses")
     .then((response) => setActor(response.data))
     
   }
 
-  useEffect(fecthActor, [])
+  useEffect(fetchActor, [])
 
   return (
     <>
-
+      <ul>
+        {actors.map((actor) =>(
+          <li key={actor.id}>
+            <h4>{actor.name}</h4>
+            <p>{actor.birth_year}</p>
+            <p>{actor.nationality}</p>
+            <p>{actor.most_famous_movies}</p>
+            <p>{actor.awards}</p>
+            <p>{actor.biography}</p>
+            <img src={actor.image} alt={actor.name} />
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
