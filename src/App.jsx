c4e870790f7f808f54be79675a6dfd68015e2b7b
severogianluca <gianlucasevero300@gmail.com>
2025-04-13 23:57:36 +0200

    Feat: add actresses list

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,70 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [actors, setActor] = useState([])
+  const [actors, setActors] = useState([]);
+  const [actresses, setActresses] = useState([]);
 
   function fetchActor() {
-    axios.get("https://www.freetestapi.com/api/v1/actresses")
-      .then((response) => setActor(response.data))
+    axios
+      .get("https://www.freetestapi.com/api/v1/actors")
+      .then((response) => setActors(response.data));
   }
 
-  useEffect(fetchActor, [])
+  function fetchActresses() {
+    axios
+      .get("https://www.freetestapi.com/api/v1/actresses")
+      .then((response) => setActresses(response.data));
+  }
+
+  useEffect(fetchActor, []);
+  useEffect(fetchActresses, []);
 
   return (
     <>
-    <div>
-      <h1>Attrici</h1>
-    </div>
+      <div>
+        <h1>Attori</h1>
+      </div>
       <div className="card-group">
         {actors.map((actor) => (
           <div key={actor.id} className="card m-2" style={{ minWidth: "18rem" }}>
             <img src={actor.image} className="card-img-top actor-img" alt={actor.name} />
             <div className="card-body">
               <h5 className="card-title">{actor.name}</h5>
-              <p className="card-text">Born in{actor.birth_year}</p>
+              <p className="card-text">Born in {actor.birth_year}</p>
               <p className="card-text">{actor.most_famous_movies}</p>
               <p className="card-text">{actor.awards}</p>
               <p className="card-text">{actor.biography}</p>
               <p className="card-text">
-                <small className="text-body-secondary">{actor.nationality}</small>
+                <small>{actor.nationality}</small>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <h1>Attrici</h1>
+      </div>
+      <div className="card-group">
+        {actresses.map((actres) => (
+          <div key={actres.id} className="card m-2" style={{ minWidth: "18rem" }}>
+            <img src={actres.image} className="card-img-top actor-img" alt={actres.name} />
+            <div className="card-body">
+              <h5 className="card-title">{actres.name}</h5>
+              <p className="card-text">Born in {actres.birth_year}</p>
+              <p className="card-text">{actres.most_famous_movies}</p>
+              <p className="card-text">{actres.awards}</p>
+              <p className="card-text">{actres.biography}</p>
+              <p className="card-text">
+                <small>{actres.nationality}</small>
               </p>
             </div>
           </div>
         ))}
       </div>
     </>
-
-      )
+  );
 }
 
-      export default App
-
-
-{/* <>
-   <ul>
-     {actors.map((actor) =>(
-      <li key={actor.id}>
-        <h4>{actor.name}</h4>
-        <p>{actor.birth_year}</p>
-        <p>{actor.nationality}</p>
-        <p>{actor.most_famous_movies}</p>
-        <p>{actor.awards}</p>
-        <p>{actor.biography}</p>
-        <img src={actor.image} alt={actor.name} />
-      </li>
-    ))}
-  </ul>
-</> */}
+export default App;
